d1ae41003f53fe94956b54c0b5b4a03909698c85
Mauricioam <mauricioam91@gmail.com>
2022-12-21 13:48:18 -0300

    axios error in response

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -4,61 +4,69 @@ import { MagnifyIcon } from "../Icons";
 
 export const API_KEY = "bb4a906819197c0bec517509eea8775a";
 
-function Header({setCity,setLoading,isLoading}){
+function Header({ setCity, setLoading, isLoading }) {
+  const [input, setInput] = useState([]);
 
-    const [input, setInput] = useState([]);
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input.length) {
+      alert("Please enter a city");
+      return;
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!input.length){
-            alert("Please enter a city");
-            return
+    setLoading(true);
+    console.log(input);
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${
+          input && input
+        }&appid=${API_KEY}`
+      )
+      .then((res) => {
+        if (res.data.length > 0) {
+          console.log(res.data);
+          setCity(res.data);
+          setLoading(false);
+        } else {
+          alert("Enter a valid city");
+          setLoading(false);
         }
-        
-        setLoading(true);
-        console.log(input)
-        axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${input && input}&appid=${API_KEY}`)
-        .then(res => {
-          if(res.data.length > 0){
-            console.log(res.data)
-            setCity(res.data)
-            setLoading(false);
-          } else {
-            alert("Enter a valid city")
-            setLoading(false);
-          }
-        })
-        .catch(error => console.log(error));
-        
+      })
+      .catch((error) => console.log(error.response.data));
+  };
 
-    }
-    
-    const handleInput = (e) =>  {
-        const inputValue = e.currentTarget.value;
-        setInput(inputValue);
-    }
-    return (
-        <>
-        <header className="row justify-content-center">
-        <form className=" col-lg-4 col-md-4 col-sm-8 col-10 justify-content-center d-flex gap-3  p-4" onSubmit={handleSubmit}  >
-        <input
-        onChange={handleInput}
-         autoComplete="off"
-          className="form-control ms-2 input"
-          type="search"
-          placeholder="Enter city..."
-          aria-label="Search"
-          name="city"
-        />
+  const handleInput = (e) => {
+    const inputValue = e.currentTarget.value;
+    setInput(inputValue);
+  };
+  return (
+    <>
+      <header className="row justify-content-center">
+        <form
+          className=" col-lg-4 col-md-4 col-sm-8 col-10 justify-content-center d-flex gap-3  p-4"
+          onSubmit={handleSubmit}
+        >
+          <input
+            onChange={handleInput}
+            autoComplete="off"
+            className="form-control ms-2 input"
+            type="search"
+            placeholder="Enter city..."
+            aria-label="Search"
+            name="city"
+          />
 
-        <button className="btn btn-outline-warning d-flex align-items-center justify-content-center" type="submit" disabled={isLoading}>
-          <MagnifyIcon/>
-        </button>
-      </form>
+          <button
+            className="btn btn-outline-warning d-flex align-items-center justify-content-center"
+            type="submit"
+            disabled={isLoading}
+          >
+            <MagnifyIcon />
+          </button>
+        </form>
       </header>
-        </>
-    )
+    </>
+  );
 }
 
 export default Header;
